edffe9ed1c6e472af2638263a838a8fb6d9a73c8
one-moonman <nicola.manoilov@gmail.com>
2023-04-22 18:29:23 +0200

    rename to tiler

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,5 @@
 <script>
-    import { onMount } from "svelte";
+    import { onDestroy, onMount } from "svelte";
     import GiAbstract080 from "svelte-icons/gi/GiAbstract080.svelte";
 
     let tiles = [];
@@ -9,13 +9,14 @@
         tileSize: 0,
     };
 
+    const min = 9;
+    const max = 14;
+
     function layout() {
         const surface = {
             width: document.documentElement.clientWidth,
             height: document.documentElement.clientHeight,
         };
-        const min = 9;
-        const max = 14;
 
         let areaWidth = surface.width;
         let columnCount = min;
@@ -27,28 +28,44 @@
                 areaWidth--;
             }
         }
-        measurements.colCount = columnCount;
+
+        // add one additional column and two rows
+        // in case of offseting the grid position when fixed, for more unfixed pattern
+
+        measurements.colCount = columnCount + 1;
         measurements.tileSize = areaWidth / columnCount;
         measurements.rowCount =
-            Math.floor(surface.height / measurements.tileSize) + 1;
-        tiles = new Array(measurements.rowCount).fill(new Array(columnCount));
+            Math.floor(surface.height / measurements.tileSize) + 2;
+
+        tiles = new Array(measurements.rowCount).fill(
+            new Array(measurements.colCount)
+        );
     }
 
     onMount(() => {
         layout();
         window.addEventListener("resize", layout);
     });
+
+    onDestroy(() => window.removeEventListener("resize", layout));
 </script>
 
 <div
     style={`
+        top: -${measurements.tileSize / 2}px;
+        left: -${measurements.tileSize / 2}px;
+        position: fixed;
         display: grid;
-        grid-template-columns:  repeat(${measurements.colCount}, ${measurements.tileSize}px);
-        grid-template-rows:  repeat(${measurements.rowCount}, ${measurements.tileSize}px);
+        grid-template-columns:  repeat(${measurements.colCount}, ${
+        measurements.tileSize
+    }px);
+        grid-template-rows:  repeat(${measurements.rowCount}, ${
+        measurements.tileSize
+    }px);
     `}
 >
     {#each tiles as row, y}
-        {#each row as column, x}
+        {#each row as _, x}
             {#if (x + y) % 2 === 0}
                 <GiAbstract080 />
             {:else}
